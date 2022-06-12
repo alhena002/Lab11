e0bbf9fa943f75afa165fd5cf27fb9d2f69399c6
Jorge <jorge.carpena@tecsup.edu.pe>
2022-06-11 20:05:07 -0500

    Agregar archivos de proyecto.

---
+++ Lab11/Migrations/202206120055321_v3.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab11.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v3 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.OfficeAssignments", "Activo", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.OfficeAssignments", "Activo");
+        }
+    }
+}
---
+++ Lab11/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace Lab11.Models
+{
+    public class Student : Person
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Required]
+        public bool Activo { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
